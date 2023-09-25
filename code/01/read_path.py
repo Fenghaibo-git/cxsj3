dcfc691d672113741c65a75904d90aa1e1e66c78
fhb <12010505@mail.sustech.edu.cn>
2023-09-25 16:31:16 +0800

    查找路径添加排序功能

--- code/01/read_path.py
+++ code/01/read_path.py
@@ -6,6 +6,8 @@ def read_xml_path(in_path):
     for file in os.listdir(in_path):
         if os.path.splitext(file)[1] == '.xml':
             xml_path_list.append(os.path.join(in_path, file))
+    # 排序
+    xml_path_list.sort()
     return xml_path_list
 
 # 读取文件夹中所有nii文件的路径
@@ -14,6 +16,8 @@ def read_nii_path(in_path):
     for file in os.listdir(in_path):
         if os.path.splitext(file)[1] == '.nii':
             nii_path_list.append(os.path.join(in_path, file))
+    # 排序
+    nii_path_list.sort()
     return nii_path_list
 
 if __name__ == '__main__':
